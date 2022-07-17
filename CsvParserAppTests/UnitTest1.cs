e8debb0b46da990338addd0c069c8bf7805b6407
Hayley96 <h.jonas@outlook.com>
2022-07-17 08:17:26 +0100

    First commit from local - setup initial default app and testing frameworks.

---
+++ CsvParserAppTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CsvParserAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
